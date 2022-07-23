6f23c366169f9a5da9badadc8496ef9a6f7c029b
Seevakan1997 <seevakan1@gmail.com>
2022-07-23 09:33:40 +0530

    Resturent

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Logo from './img/logo.png'
 
 const Header = () => {
   return (
-    <div className='fixed z-50 w-screen bg-slate-300 p-6 px-16'>Header</div>
+    <header className='fixed z-50 w-screen p-6 px-16'>
+     {/* desktop and Tab */}
+      <div className='hidden md:flex w-full h-full'> 
+        <div className='flex items-center gap-2'>
+          <img src={Logo} alt='logo' className='w-8 object-cover'/>
+          <p className='text-headingColor text-xl font-bold'>City</p>
+        </div>
+        
+        <ul className='flex items-center gap-8 ml-auto'>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>About Us</li>
+          <li>Service</li>
+        </ul>
+      </div>
+     {/* Mobile */}
+      <div className='flex md:hidden w-full h-full'></div>
+    </header>
   )
 }
 
