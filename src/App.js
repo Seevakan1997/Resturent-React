f8eae0202f00a101d40754e0ff89cd68a328d1d1
Seevakan1997 <seevakan1@gmail.com>
2022-07-23 10:40:52 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from './components'
+import { Route,Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
+    <div className='w-screen h-auto flex flex-col bg-primary'>
       <Header/>
+      <main className='mt-24 p-8 w-full'>Main container</main>
     </div>
   )
 }
